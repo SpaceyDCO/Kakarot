2ecadf15df53e74990f0d188ac0a0feaa43f8f8e
SpaceyDCO <spaidytroll.gamer@gmail.com>
2025-08-02 23:26:58 -0400

    Arena.java created Scenario.java created SpawnInfo.java created Wave.java created

---
+++ src/github/kakarot/Raids/Arena.java
@@ -0,0 +1,25 @@
+package github.kakarot.Raids;
+
+import github.kakarot.Raids.Helpers.SerializableLocation;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@NoArgsConstructor
+@Getter
+public class Arena {
+    private String arenaName;
+    private String worldName;
+    private String scenarioName;
+    private SerializableLocation playerSpawnLocation;
+    private final Map<String, SerializableLocation> npcSpawnPoints = new HashMap<>();
+    private SerializableLocation spectatorSpawn;
+    private SerializableLocation boundaryCorner1;
+    private SerializableLocation boundaryCorner2;
+
+    public SerializableLocation getNpcSpawnPoint(String name) {
+        return this.npcSpawnPoints.getOrDefault(name, null);
+    }
+}
---
+++ src/github/kakarot/Raids/Scenario/Wave.java
@@ -0,0 +1,25 @@
+package github.kakarot.Raids.Scenario;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+public class Wave {
+    /**
+     * "waveNumber" value from .json file
+     * "waveNumber": 1 -> waveNumber is 1
+     * "waveNumber": 7 -> waveNumber is 7
+     */
+    private int waveNumber;
+    /**
+     * Message to send the players when this wave starts
+     */
+    private String startMessage;
+    /**
+     * A list containing all Spawning information (npc id, coordinates)
+     */
+    private List<SpawnInfo> spawns;
+}
