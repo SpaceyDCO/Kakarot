14e00f89d1392fe73cd0e3216fed094ee7a1a2a5
SpaceyDCO <spaidytroll.gamer@gmail.com>
2026-01-29 19:46:21 -0400

    Add PlayerProgressManager to refactor methods from QuestManager later, at this moment only 2 methods were refactored

---
+++ src/github/kakarot/Quests/Managers/PlayerProgressManager.java
@@ -0,0 +1,44 @@
+package github.kakarot.Quests.Managers;
+
+import github.kakarot.Main;
+import github.kakarot.Quests.Models.PlayerQuestProgress;
+import org.bukkit.Bukkit;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class PlayerProgressManager {
+    private final Map<UUID, Map<Integer, PlayerQuestProgress>> playerProgressMap = new HashMap<>();
+    private final Main plugin;
+    public PlayerProgressManager(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    /**
+     * Loads a player's progress from database to cache
+     * Safe to call on main thread
+     * @param playerUUID The player's uuid whose progress will be loaded to cache
+     */
+    public void loadPlayerProgress(UUID playerUUID) {
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+            Map<Integer, PlayerQuestProgress> progress = QuestDB.getPlayerQuests(this.plugin, playerUUID);
+            this.playerProgressMap.put(playerUUID, progress);
+            plugin.getLogger().info("Loaded quest progress for player " + Bukkit.getPlayer(playerUUID).getName() + ".\n" + progress.size() + " progressed quests loaded.");
+        });
+    }
+
+    /**
+     * Saves a player's progress to database and removes it from cache
+     * Currently only removes from cache as database is updated on the go
+     * Meant to be called when a player quits the server
+     * Safe to call on main thread
+     * @param playerUUID The player's uuid whose progress will be "moved" to database
+     */
+    public void savePlayerProgress(UUID playerUUID) {
+        this.playerProgressMap.computeIfPresent(playerUUID, (key, oldValue) -> {
+            plugin.getLogger().info("Cleared progress for player " + Bukkit.getPlayer(playerUUID).getName() + "\n" + oldValue.size() + " quests removed from cache.");
+            return this.playerProgressMap.remove(key);
+        });
+    }
+}
