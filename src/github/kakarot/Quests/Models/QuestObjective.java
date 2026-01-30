d898bb2c860cac18c00ec4c0b4c0482f44f46197
SpaceyDCO <spaidytroll.gamer@gmail.com>
2026-01-30 00:36:49 -0400

    Changed field target to a reference to ObjectiveInfo

--- src/github/kakarot/Quests/Models/QuestObjective.java
+++ src/github/kakarot/Quests/Models/QuestObjective.java
@@ -2,12 +2,13 @@ package github.kakarot.Quests.Models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.jspecify.annotations.Nullable;
 
 @Getter
 @AllArgsConstructor
 public class QuestObjective {
     private final ObjectiveType type;
-    private String target;
+    private ObjectiveInfo objectiveInfo;
     private final int required;
     private final boolean shareable;
     public boolean isComplete(int currentProgress) {
