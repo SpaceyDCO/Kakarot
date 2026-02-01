1ca7f492ea973bb41015d58bbb9bc51c12fd3b21
SpaceyDCO <spaidytroll.gamer@gmail.com>
2026-02-01 15:57:09 -0400

    Add method playerHasRequiredItem()

--- src/github/kakarot/Quests/Managers/PlayerProgressManager.java
+++ src/github/kakarot/Quests/Managers/PlayerProgressManager.java
@@ -1,8 +1,16 @@
 package github.kakarot.Quests.Managers;
 
 import github.kakarot.Main;
+import github.kakarot.Quests.Models.ObjectiveInfo;
 import github.kakarot.Quests.Models.PlayerQuestProgress;
+import github.kakarot.Quests.Models.QuestObjective;
+import github.kakarot.Tools.NbtHandler;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -37,5 +45,94 @@ public class PlayerProgressManager {
      */
     public void savePlayerProgress(UUID playerUUID) {
         this.playerProgressMap.remove(playerUUID);
+    }
+    public boolean playerHasRequiredItem(Player player, QuestObjective objective) {
+        ObjectiveInfo info = objective.getObjectiveInfo();
+        int itemId = info.getItemId();
+        byte dataValue = info.getDataValue();
+        String requiredNbt = info.getNbt();
+        NBTTagCompound requiredCompound = info.getParsedNbt() != null ? info.getParsedNbt() : null;
+        int count = 0;
+        for(ItemStack item : player.getInventory().getContents()) {
+            if(item == null || item.getTypeId() != itemId) continue;
+            if(item.getData().getData() != dataValue) continue;
+            if(requiredNbt != null && !requiredNbt.isEmpty()) {
+                if(!itemMatchesNbt(item, requiredCompound)) continue;
+            }
+            count += item.getAmount();
+            if(count >= objective.getRequired()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean itemMatchesNbt(ItemStack item, NBTTagCompound requiredNbt) {
+        NbtHandler handler = new NbtHandler(item);
+        if(!handler.hasNBT() && requiredNbt != null) return false; //Item HAS NOT NBT, but the objective requires NBT
+        return nbtContainsAllTags(handler.getCompound(), requiredNbt);
+    }
+    private boolean nbtContainsAllTags(NBTTagCompound actual, NBTTagCompound required) {
+        if(actual == null || required == null) return false; //Just in case
+        for(Object keyObj : required.func_150296_c()) {
+            String key = (String) keyObj;
+            if(!actual.hasKey(key)) return false;
+            NBTBase requiredTag = required.getTag(key);
+            NBTBase actualTag = actual.getTag(key);
+            if(requiredTag instanceof NBTTagCompound && actualTag instanceof NBTTagCompound) {
+                if(!nbtContainsAllTags((NBTTagCompound) actualTag, (NBTTagCompound) requiredTag)) return false;
+            }else if(requiredTag instanceof NBTTagList && actualTag instanceof NBTTagList) {
+                if(!nbtListMatches((NBTTagList) actualTag, (NBTTagList) requiredTag)) return false;
+            }else {
+                if(!requiredTag.equals(actualTag)) return false;
+            }
+        }
+        return true;
+    }
+    private boolean nbtListMatches(NBTTagList actual, NBTTagList required) {
+        if (actual.tagCount() < required.tagCount()) return false;
+        int tagType = required.func_150303_d();
+        // Handle compound lists (enchantments)
+        if (tagType == 10) {
+            for (int i = 0; i < required.tagCount(); i++) {
+                NBTTagCompound requiredElement = required.getCompoundTagAt(i);
+
+                boolean found = false;
+                for (int j = 0; j < actual.tagCount(); j++) {
+                    NBTTagCompound actualElement = actual.getCompoundTagAt(j);
+                    if (nbtContainsAllTags(actualElement, requiredElement)) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) return false;
+            }
+            return true;
+        }
+        // Handle string lists (Lore)
+        else if (tagType == 8) {
+            for (int i = 0; i < required.tagCount(); i++) {
+                String requiredStr = required.getStringTagAt(i);
+                boolean found = false;
+                for (int j = 0; j < actual.tagCount(); j++) {
+                    String actualStr = actual.getStringTagAt(j);
+
+                    if (requiredStr.equals(actualStr)) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) return false;
+            }
+            return true;
+        }
+        else {
+            // For safety, do string comparison on list elements
+            for (int i = 0; i < required.tagCount(); i++) {
+                String reqStr = required.toString();
+                String actStr = actual.toString();
+                if (!actStr.contains(reqStr)) return false;
+            }
+            return true;
+        }
     }
 }
