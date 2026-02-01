5e05a62b5ab4509aa89f48b68ebf61e52604376f
SpaceyDCO <spaidytroll.gamer@gmail.com>
2026-02-01 15:56:57 -0400

    Add a parsed NBT to ObjectiveInfo for optimization purposes

--- src/github/kakarot/Quests/Models/ObjectiveInfo.java
+++ src/github/kakarot/Quests/Models/ObjectiveInfo.java
@@ -1,13 +1,38 @@
 package github.kakarot.Quests.Models;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import net.minecraft.nbt.JsonToNBT;
+import net.minecraft.nbt.NBTException;
+import net.minecraft.nbt.NBTTagCompound;
 
-@AllArgsConstructor @Getter
+@Getter
 public class ObjectiveInfo {
     private final String target;
     private String title;
+    private String nbt;
+    private NBTTagCompound parsedNbt;
+    public ObjectiveInfo(String target, String title, String nbt) throws NBTException {
+        this.target = target;
+        this.title = title;
+        this.nbt = nbt;
+        if(nbt != null && !nbt.isEmpty()) this.parsedNbt = (NBTTagCompound) JsonToNBT.func_150315_a(nbt);
+    }
     public ObjectiveInfo(String target) {
         this.target = target;
+    }
+    public ObjectiveInfo(String target, String title) {
+        this.target = target;
+        this.title = title;
+    }
+    public int getItemId() {
+        if(!target.contains(":")) return Integer.parseInt(target);
+        return Integer.parseInt(target.split(":")[0]);
+    }
+    public byte getDataValue() {
+        if(!target.contains(":")) return 0;
+        return Byte.parseByte(target.split(":")[1]);
+    }
+    public boolean hasNbt() {
+        return nbt != null && !nbt.isEmpty();
     }
 }
