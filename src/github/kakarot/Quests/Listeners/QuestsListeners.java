157047a1ad93145706f2fb11be543ebfbd3ac838
SpaceyDCO <spaidytroll.gamer@gmail.com>
2026-01-31 22:04:56 -0400

    add method onPlayerNpcInteract to use when players interact with an npc

--- src/github/kakarot/Quests/Listeners/QuestsListeners.java
+++ src/github/kakarot/Quests/Listeners/QuestsListeners.java
@@ -3,10 +3,12 @@ package github.kakarot.Quests.Listeners;
 import github.kakarot.Main;
 import github.kakarot.Quests.Managers.PlayerProgressManager;
 import github.kakarot.Quests.Managers.QuestManager;
+import github.kakarot.Quests.Models.ObjectiveType;
 import noppes.npcs.api.entity.ICustomNpc;
 import noppes.npcs.api.entity.IEntity;
 import noppes.npcs.api.entity.IPlayer;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,7 +17,6 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 public class QuestsListeners implements Listener {
@@ -32,8 +33,11 @@ public class QuestsListeners implements Listener {
     }
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        progressManager.savePlayerProgress(event.getPlayer().getUniqueId());
+        OfflinePlayer player = Bukkit.getOfflinePlayer(event.getPlayer().getUniqueId());
+        if(player == null) return;
+        progressManager.savePlayerProgress(player.getUniqueId());
     }
+
     public void onNpcDied(ICustomNpc<?> npc, IEntity<?> killer) {
         if(npc == null || killer == null) {
             plugin.getLogger().warning("Critical ERROR: NPC or Player killer with null value");
@@ -42,15 +46,37 @@ public class QuestsListeners implements Listener {
         if(!(killer instanceof IPlayer<?>)) return;
         IPlayer<?> iPlayer = (IPlayer<?>) killer;
         Player player = Bukkit.getPlayer(UUID.fromString(iPlayer.getUniqueID()));
-        List<QuestManager.QuestObjectiveReference> references = plugin.getQuestManager().npcKillObjectives.getOrDefault(npc.getName(), new ArrayList<>());
+        List<QuestManager.QuestObjectiveReference> references = plugin.getQuestManager().npcObjectives.getOrDefault(npc.getName(), new ArrayList<>());
+        checkReferenceAndProgressObjective(references, player.getUniqueId(), npc, ObjectiveType.KILL_MOBS, 1);
+    }
+    public void onPlayerNpcInteract(ICustomNpc<?> npc, IPlayer<?> iPlayer) {
+        if(npc == null || iPlayer == null) {
+            plugin.getLogger().warning("Critical ERROR: Npc or Player interact with null value");
+            return;
+        }
+        Player player = Bukkit.getPlayer(UUID.fromString(iPlayer.getUniqueID()));
+        List<QuestManager.QuestObjectiveReference> references = plugin.getQuestManager().npcObjectives.getOrDefault(npc.getName(), new ArrayList<>());
+        checkReferenceAndProgressObjective(references, player.getUniqueId(), npc, ObjectiveType.TALK_TO_NPC, 1);
+    }
+
+    /**
+     * Using reverse index, checks if the given NPC is linked to a quest, then progresses the player by amount
+     * @param references The list of QuestObjectiveReference to do the reverse index search in
+     * @param playerUUID The player doing the action (killing or interacting with an npc)
+     * @param npc The npc being actioned on
+     * @param amount The amount of objective value to progress
+     */
+    private void checkReferenceAndProgressObjective(List<QuestManager.QuestObjectiveReference> references, UUID playerUUID, ICustomNpc<?> npc, ObjectiveType ACTION, int amount) {
         if(references.isEmpty()) return;
         for(QuestManager.QuestObjectiveReference reference : references) {
-            if(plugin.getQuestManager().hasPickedUpQuest(player.getUniqueId(), reference.getQuestId())) {
+            if(reference.getType() != ACTION) continue;
+            int questId = reference.getQuestId();
+            if(plugin.getQuestManager().hasPickedUpQuest(playerUUID, questId)) {
+                if(plugin.getQuestManager().hasCompletedQuest(playerUUID, reference.getQuestId()) || plugin.getQuestManager().hasCompletedObjective(playerUUID, reference.getQuestId(), reference.getObjectiveIndex())) continue;
                 if(!reference.getTitle().isEmpty() && npc.getTitle().equals(reference.getTitle())) {
-                    if(!plugin.getQuestManager().hasCompletedQuest(player.getUniqueId(), reference.getQuestId())) plugin.getQuestManager().progressObjective(player.getUniqueId(), reference.getQuestId(), reference.getObjectiveIndex(), 1);
-                }
-                else if(reference.getTitle().isEmpty()) {
-                    if(!plugin.getQuestManager().hasCompletedQuest(player.getUniqueId(), reference.getQuestId())) plugin.getQuestManager().progressObjective(player.getUniqueId(), reference.getQuestId(), reference.getObjectiveIndex(), 1);
+                    plugin.getQuestManager().progressObjective(playerUUID, reference.getQuestId(), reference.getObjectiveIndex(), amount);
+                }else if(reference.getTitle().isEmpty()) {
+                    plugin.getQuestManager().progressObjective(playerUUID, reference.getQuestId(), reference.getObjectiveIndex(), amount);
                 }
             }
         }
