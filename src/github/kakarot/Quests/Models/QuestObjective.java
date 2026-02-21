c01e22530b2f0d6876f6d43e110e643dbd87c367
SpaceyDCO <spaidytroll.gamer@gmail.com>
2026-02-21 17:15:36 -0400

    Changes to QuestObjective constructor to handle new TrackingInfo field

--- src/github/kakarot/Quests/Models/QuestObjective.java
+++ src/github/kakarot/Quests/Models/QuestObjective.java
@@ -5,12 +5,36 @@ import lombok.Getter;
 import org.jspecify.annotations.Nullable;
 
 @Getter
-@AllArgsConstructor
 public class QuestObjective {
     private final ObjectiveType type;
     private ObjectiveInfo objectiveInfo;
+    private TrackingInfo trackingInfo;
     private final int required;
     private final boolean shareable;
+    public QuestObjective(ObjectiveType type, ObjectiveInfo objectiveInfo, int required, boolean shareable) {
+        this.type = type;
+        this.objectiveInfo = objectiveInfo;
+        this.trackingInfo = null;
+        this.required = required;
+        this.shareable = shareable;
+    }
+    public QuestObjective(ObjectiveType type, ObjectiveInfo objectiveInfo, TrackingInfo trackingInfo, int required, boolean shareable) {
+        this.type = type;
+        this.objectiveInfo = objectiveInfo;
+        TrackingInfo trackInfo = new TrackingInfo();
+        trackInfo.setX(trackingInfo.getX());
+        trackInfo.setY(trackingInfo.getY());
+        trackInfo.setZ(trackingInfo.getZ());
+        if(trackingInfo.getLabel() == null || trackingInfo.getLabel().isEmpty()) trackInfo.setLabel("");
+        else trackInfo.setLabel(trackingInfo.getLabel());
+        if(trackingInfo.getArrowColor() == null || trackingInfo.getArrowColor().isEmpty()) trackInfo.setArrowColor("white");
+        else trackInfo.setArrowColor(trackingInfo.getArrowColor());
+        if(trackingInfo.getLabelColor() == null || trackingInfo.getLabelColor().isEmpty()) trackInfo.setLabelColor("white");
+        else trackInfo.setLabelColor(trackingInfo.getLabelColor());
+        this.trackingInfo = trackInfo;
+        this.required = required;
+        this.shareable = shareable;
+    }
     public boolean isComplete(int currentProgress) {
         return currentProgress >= this.required;
     }
