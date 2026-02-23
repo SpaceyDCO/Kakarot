33d8f60c41aa82f7128aeb4bea5532cec4f1b0f2
SpaceyDCO <spaidytroll.gamer@gmail.com>
2026-02-22 23:50:50 -0400

    Update TrackingInfo model to hold a spanish and english label. Also added 2 placeholders for the label, %current_progress% and %required%

--- src/github/kakarot/Quests/Models/TrackingInfo.java
+++ src/github/kakarot/Quests/Models/TrackingInfo.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class TrackingInfo {
     private int x;
     private int y;
     private int z;
-    private String label;
+    private Map<String, String> label;
     private String arrowColor;
     private String labelColor;
 }
