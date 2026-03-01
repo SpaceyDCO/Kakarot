50364705b9a934f2863ce38eef0e5174ce08a8ae
SpaceyDCO <spaidytroll.gamer@gmail.com>
2026-03-01 16:21:53 -0400

    Add Phasing cache (to cache phasing npcs) and config manager (to load phasing npcs from yml to cache)

---
+++ src/github/kakarot/Phasing/PhasingCache.java
@@ -0,0 +1,19 @@
+package github.kakarot.Phasing;
+
+import github.kakarot.Phasing.Models.PhasedNPC;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhasingCache {
+    private static final Map<String, PhasedNPC> cache = new HashMap<>();
+    public static void add(PhasedNPC npc) {
+        cache.put(npc.getId(), npc);
+    }
+    public static Map<String, PhasedNPC> getAll() {
+        return cache;
+    }
+    public static void clear() {
+        cache.clear();
+    }
+}
---
+++ src/github/kakarot/Phasing/PhasingConfigManager.java
@@ -0,0 +1,69 @@
+package github.kakarot.Phasing;
+
+import github.kakarot.Main;
+import github.kakarot.Phasing.Models.PhasedNPC;
+import lombok.AllArgsConstructor;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.logging.Level;
+
+@AllArgsConstructor
+public class PhasingConfigManager {
+    private final Main plugin;
+    public void loadPhasedNPCsToCache() {
+        PhasingCache.clear();
+        File phasingDataFolder = new File(plugin.getDataFolder(), "phasing");
+        File phasingDataFile = new File(phasingDataFolder, "phasingData.yml");
+        if(!phasingDataFolder.exists()) {
+            if(!phasingDataFolder.mkdirs()) {
+                this.plugin.getLogger().severe("Couldn't create phasing datafolder in " + phasingDataFolder.getAbsolutePath());
+                return;
+            }
+        }
+        if(!phasingDataFile.exists()) {
+            try {
+                Files.copy(plugin.getResource("phasing/phasingData.yml"), phasingDataFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                plugin.getLogger().info("Created default resource: " + phasingDataFile.getAbsolutePath());
+            }catch(IOException e) {
+                plugin.getLogger().log(Level.SEVERE, "Could not save default resource: " + "phasing/phasingData.yml", e);
+                return;
+            }
+        }
+        try {
+            YamlConfiguration phasingFileConfig = loadYamlUtf8(phasingDataFile);
+            if(!phasingFileConfig.contains("npcs")) return;
+            ConfigurationSection npcSection = phasingFileConfig.getConfigurationSection("npcs");
+            for(String key : npcSection.getKeys(false)) {
+                try {
+                    String name = npcSection.getString(key + ".name");
+                    String title = npcSection.getString(key + ".title");
+                    String permission = npcSection.getString(key + ".permission");
+                    if(name == null || permission == null) {
+                        plugin.getLogger().warning("Skipping invalid NPC within yml phasingData: " + key + ". Missing name or permission.");
+                        continue;
+                    }
+                    PhasingCache.add(new PhasedNPC(key, name, title, permission));
+                }catch(Exception e) {
+                    plugin.getLogger().log(Level.SEVERE, "Failed to load NPC: " + key + ". Formatting error.", e);
+                }
+            }
+            plugin.getLogger().info("Successfully loaded " + PhasingCache.getAll().size() + " NPCs for Phasing System...");
+        }catch(IOException | InvalidConfigurationException e) {
+            plugin.getLogger().log(Level.SEVERE, "Invalid configuration for file phasingData.yml inside " + phasingDataFolder.getAbsolutePath(), e);
+        }
+    }
+    private YamlConfiguration loadYamlUtf8(File file) throws IOException, InvalidConfigurationException {
+        YamlConfiguration config = new YamlConfiguration();
+        try(InputStream in = Files.newInputStream(file.toPath());
+            Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8)) {
+            config.load(reader);
+        }
+        return config;
+    }
+}
